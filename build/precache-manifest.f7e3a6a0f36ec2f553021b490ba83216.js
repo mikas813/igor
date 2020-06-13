0e8440fe0782dc9b739c72c2f129ba576d21fbb5
mikas813 <mishalas@icloud.com>
2020-06-13 16:45:05 +0100

    --

---
+++ build/precache-manifest.f7e3a6a0f36ec2f553021b490ba83216.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc149b3cd7dc03f3052af3121dc020a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b6a9fcafd08342ed5c",
+    "url": "/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "dd62ef4e6fd1054787e2",
+    "url": "/static/css/main.a43f311d.chunk.css"
+  },
+  {
+    "revision": "a3b6a9fcafd08342ed5c",
+    "url": "/static/js/2.98705a90.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.98705a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd62ef4e6fd1054787e2",
+    "url": "/static/js/main.381fb359.chunk.js"
+  },
+  {
+    "revision": "f30b0492f9d14a607cc3",
+    "url": "/static/js/runtime-main.b8a6bb9a.js"
+  },
+  {
+    "revision": "c668f90a8116553057ea7c0c4b5d9094",
+    "url": "/static/media/1.c668f90a.png"
+  },
+  {
+    "revision": "975436f6eb7fb12792cd0c266bef45ae",
+    "url": "/static/media/2.975436f6.png"
+  },
+  {
+    "revision": "ffb2835185587102697346d424ccd892",
+    "url": "/static/media/3.ffb28351.png"
+  },
+  {
+    "revision": "4a0abc5438746e9ec98cd5a652a54d05",
+    "url": "/static/media/about-bg.4a0abc54.png"
+  },
+  {
+    "revision": "d4c89e9311dd4d0db2494110346c9287",
+    "url": "/static/media/banner-bg.d4c89e93.png"
+  },
+  {
+    "revision": "ecb50982c8111ba4134adb7fd26440cd",
+    "url": "/static/media/iphone-screen-repair.ecb50982.jpg"
+  },
+  {
+    "revision": "89b703841b00513a1eded00c528cea3e",
+    "url": "/static/media/menu-bg.89b70384.png"
+  }
+]);
